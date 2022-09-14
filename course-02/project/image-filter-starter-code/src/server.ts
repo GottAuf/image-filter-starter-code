9a609191a14809f6af5f8a42493f2283a9c42137
GottAuf <gordiefx@gmail.com>
2022-09-14 20:02:04 +0300

    Changes to be committed: 	new file:   .vs/ProjectSettings.json 	new file:   .vs/VSWorkspaceState.json 	new file:   .vs/image-filter-starter-code/FileContentIndex/33c120ad-e2ce-499b-a29e-28e0173dcbe5.vsidx 	new file:   .vs/image-filter-starter-code/FileContentIndex/3aaee82a-7c03-49d0-b219-e2314424aaf2.vsidx 	new file:   .vs/image-filter-starter-code/FileContentIndex/40add303-70cc-47cf-8852-e829fa7f278c.vsidx 	new file:   .vs/image-filter-starter-code/FileContentIndex/a015889b-cc93-4517-8ca5-d2867fa44a3d.vsidx 	new file:   .vs/image-filter-starter-code/FileContentIndex/read.lock 	new file:   .vs/image-filter-starter-code/config/applicationhost.config 	new file:   .vs/image-filter-starter-code/v17/.suo 	new file:   .vs/slnx.sqlite 	deleted:    deployment_screenshots/EXAMPLE_PLEASE_MAKE_YOUR_OWN.png 	new file:   deployment_screenshots/ebEvents.PNG 	new file:   deployment_screenshots/ebMaregwa.PNG 	modified:   package-lock.json 	modified:   package.json 	modified:   src/server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,6 +29,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+    app.get("/filteredimage", async (req: Request, res: Response) => {
+        const image_url = req.query.image_url;
+        if (!image_url) {
+            res.status(400).send("Image url is required");
+        }
+        const filteredImage = await filterImageFromURL(image_url);
+
+        res.status(200).sendFile(filteredImage, () => {
+            deleteLocalFiles([filteredImage])
+        });
+
+
+    });
 
   //! END @TODO1
   
